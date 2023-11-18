13435e23815c5ef9642e087f11ee92db2e070799
earlgrey02 <san06036@naver.com>
2023-11-18 15:29:31 +0900

    Feat: WebClient 로깅 설정

--- src/main/kotlin/com/quizit/backend/global/config/WebClientConfiguration.kt
+++ src/main/kotlin/com/quizit/backend/global/config/WebClientConfiguration.kt
@@ -1,11 +1,22 @@
 package com.quizit.backend.global.config
 
+import com.quizit.backend.global.util.getLogger
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.reactive.function.client.WebClient
 
 @Configuration
 class WebClientConfiguration {
+    private val logger = getLogger()
+
     @Bean
-    fun webClient(): WebClient = WebClient.create()
+    fun webClient(): WebClient =
+        WebClient.builder()
+            .filter { request, next ->
+                logger.info { "HTTP ${request.method()} ${request.url().path}" }
+
+                next.exchange(request)
+                    .doOnNext { logger.info { "HTTP ${it.statusCode()}" } }
+            }
+            .build()
 }
